57be9becaac3fc85e46fea089065857a052fae6e
Tawana <60897777+TawanaLeon@users.noreply.github.com>
2020-03-27 13:57:06 -0400

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,1 +1,20 @@
 import math
+import requests 
+import os
+
+from discord.ext import commands
+base_url= https://www.desmos.com/
+  import math
+
+                       a = 1
+                       b = 5
+                       c = 6
+
+                       d = (b**2) - (4*a*c)
+                       sol1 = (-b-cmath.sqrt(d))/(2*a)
+                       sol2 = (-b+cmath.sqrt(d))/(2*a)
+
+import discord 
+print(input equation)
+
+ctx.bot@commannd)input()
