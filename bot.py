d8f65b89b388a0b312bbd65b49196fbd24f6dfb0
ChrisRomeroo <54958454+ChrisRomeroo@users.noreply.github.com>
2020-04-08 09:47:56 -0400

    Update on bot.py

--- bot.py
+++ bot.py
@@ -61,4 +61,18 @@ def__sqrt
     
 bot.run(token)          
                  
-                
+                 
+
+import cmath
+  
+a = float(input('Enter a: '))  
+b = float(input('Enter b: '))  
+c = float(input('Enter c: '))  
+  
+# calculate the discriminant  
+d = (b**2) - (4*a*c)  
+  
+# find two solutions  
+sol1 = (-b-cmath.sqrt(d))/(2*a)  
+sol2 = (-b+cmath.sqrt(d))/(2*a)  
+print('The solutions are {0} and {1}'.format(sol1,sol2)) 
