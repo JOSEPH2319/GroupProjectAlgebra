c9d501e6cdff9cb408815c063786f92513413d4e
JOSEPH2319 <54958447+JOSEPH2319@users.noreply.github.com>
2020-04-18 23:43:09 -0400

    Update 2bot.py

--- 2bot.py
+++ 2bot.py
@@ -12,3 +12,20 @@ x1 = (-b + d)/(2 * a)
 x2 = (-b - d)/(2 * a)
 
 print(x1, x2)
+
+
+
+import matplotlib.pyplot as plt
+import time
+import numpy
+
+def plot_linear_eq(a, b, clr):
+  x = list(range(-10, 11)
+  y = [(a*i + b) for i in x]
+  plt.plot(x, y, label = 'linear', linestyle = '-', color = clr)
+
+a = 0
+b = 0
+plot_linear_eq(a, b, 'r')
+plt.show(block = False)
+plt.close
