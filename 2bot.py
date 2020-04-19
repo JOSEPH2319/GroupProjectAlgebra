90e61485aea756cb4d044c72f3bdd0257b71589b
JOSEPH2319 <54958447+JOSEPH2319@users.noreply.github.com>
2020-04-19 19:57:49 -0400

    Update 2bot.py

--- 2bot.py
+++ 2bot.py
@@ -28,4 +28,37 @@ a = 0
 b = 0
 plot_linear_eq(a, b, 'r')
 plt.show(block = False)
-plt.close
+plt.close(5)
+plt.close()
+
+           
+a = 1
+b = 0
+plot_linear_eq(a, b, 'b')
+plt.show(block = False)
+plt.pause(5)
+plt.close()
+         
+def plot_quadratic_eq(a, b, c, clr):
+  x = list(range(-10, 11))
+  y = [ (a*i**2 + b*i +c) for i in x]
+  plt.plot(x, y, label = 'linear', linestyle = '-', color = clr)
+           
+a = 1
+b = 0
+c = 0
+plot_quadratic_eq(a, b, c, 'r')
+plt.show(block = False)
+plt.pause(5)
+plt.close()
+           
+a = -1
+b = 0
+b = 0
+plot_quadratic_eq(a, b, c, 'b')
+plt.show(block = False)
+plt.pause(5)
+plt.close()
+         
+
+
