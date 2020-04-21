4c79f7f9ccd6186c6272db49541587e6eeb647ac
Tawana <60897777+TawanaLeon@users.noreply.github.com>
2020-04-20 22:03:32 -0400

    Final commit

--- bot.py
+++ bot.py
@@ -91,53 +91,6 @@ x1 = (-b + d)/(2 * a)
 x2 = (-b - d)/(2 * a)
 
 print(x1, x2)
-
-
-
-import matplotlib.pyplot as plt
-import time
-import numpy
-
-def plot_linear_eq(a, b, clr):
-  x = list(range(-10, 11)
-  y = [(a*i + b) for i in x]
-  plt.plot(x, y, label = 'linear', linestyle = '-', color = clr)
-
-a = 0
-b = 0
-plot_linear_eq(a, b, 'r')
-plt.show(block = False)
-plt.close(5)
-plt.close()
-
-           
-a = 1
-b = 0
-plot_linear_eq(a, b, 'b')
-plt.show(block = False)
-plt.pause(5)
-plt.close()
-         
-def plot_quadratic_eq(a, b, c, clr):
-  x = list(range(-10, 11))
-  y = [ (a*i**2 + b*i +c) for i in x]
-  plt.plot(x, y, label = 'linear', linestyle = '-', color = clr)
-           
-a = 1
-b = 0
-c = 0
-plot_quadratic_eq(a, b, c, 'r')
-plt.show(block = False)
-plt.pause(5)
-plt.close()
-           
-a = -1
-b = 0
-b = 0
-plot_quadratic_eq(a, b, c, 'b')
-plt.show(block = False)
-plt.pause(5)
-plt.close()
-         
  
            
+bot.run(token)          
